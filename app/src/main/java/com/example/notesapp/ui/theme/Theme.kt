72772006ef0bc3fc69bd6bb2b1f0ac035489bba5
jeronjoy12 <jeronjoy@retailcloud.com>
2026-06-05 15:44:54 +0530

    D5_task_edited

--- app/src/main/java/com/example/notesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/notesapp/ui/theme/Theme.kt
@@ -1,6 +1,6 @@
 package com.example.notesapp.ui.theme
 
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,7 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import com.example.notesapp.navigation.NotesNavGraph
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = DarkBluePrimary,
